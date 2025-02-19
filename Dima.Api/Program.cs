2125fbb55f0aff06e182e5d99b98adf71e38c82f
Filipe Pizzetti <pizzetti.filipe@gmail.com>
2025-02-18 23:01:53 -0300

    Criado mapeamentos e configuracoes para o banco de dados

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,62 @@
+using Dima.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(
+    x =>
+    {
+        x.UseSqlServer(connection);
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
+
+builder.Services.AddTransient<Handler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("/v1/transactions",
+            (Request request, Handler handler)
+            => handler.Handle(request))
+    .Produces<Response>();
+
 
 app.Run();
+
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+}
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response
+        {
+            Id = 4,
+            Title = request.Title
+        };
+    }
+}
